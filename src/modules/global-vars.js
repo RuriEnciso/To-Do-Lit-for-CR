c42756cce7df67c4a5e0e4579e7df8598a330312
RuriEnciso <rurienciso@gmail.com>
2023-06-20 08:40:34 -0500

    update src folder

--- src/modules/global-vars.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('todoform');
-const todoImput = document.getElementById('newtodo');
-const todosListEl = document.getElementById('todos-list');
-const notificationEl = document.querySelector('.notification');
-const clearAllCompleted = document.querySelector('.clear-all-btn');
-export {
-  form, todoImput, todosListEl, notificationEl, clearAllCompleted,
-};
