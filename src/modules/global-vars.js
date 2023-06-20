5933adbf04d420f0dd6d25ebe6baca784ef16cf0
RuriEnciso <rurienciso@gmail.com>
2023-06-20 08:11:08 -0500

    Added basic files like css html and js

---
+++ src/modules/global-vars.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('todoform');
+const todoImput = document.getElementById('newtodo');
+const todosListEl = document.getElementById('todos-list');
+const notificationEl = document.querySelector('.notification');
+const clearAllCompleted = document.querySelector('.clear-all-btn');
+export {
+  form, todoImput, todosListEl, notificationEl, clearAllCompleted,
+};
